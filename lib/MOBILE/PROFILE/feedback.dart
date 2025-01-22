e8256602cd39cf5ef8b15e10eaa60cbab8870eea
sayakflutter <sayak.solution27@gmail.com>
2025-01-22 19:05:45 +0530

    update

--- lib/MOBILE/PROFILE/feedback.dart
+++ lib/MOBILE/PROFILE/feedback.dart
@@ -3,7 +3,6 @@ import 'package:dthlms/THEME_DATA/font/font_family.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-
 class FeedBackMobile extends StatefulWidget {
   const FeedBackMobile({super.key});
 
@@ -15,17 +14,22 @@ class _FeedBackMobileState extends State<FeedBackMobile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
         backgroundColor: ColorPage.mainBlue,
-        title: Text('Feedback',style: FontFamily.style.copyWith(color: Colors.white),),),
-      body: Container(color: Colors.white,
-      child: FeedbackBox(),
+        title: Text(
+          'Feedback',
+          style: FontFamily.style.copyWith(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: FeedbackBox(),
       ),
     );
   }
 }
-
 
 class FeedbackBox extends StatefulWidget {
   const FeedbackBox({super.key});
@@ -37,107 +41,128 @@ class FeedbackBox extends StatefulWidget {
 class _FeedbackBoxState extends State<FeedbackBox> {
   @override
   Widget build(BuildContext context) {
-     return LayoutBuilder(
-      builder: (context, constraints) {
-        // Calculate 20% of the width for padding
-        double horizontalPadding = constraints.maxWidth * 0.05;
-        double h = constraints.maxHeight * 0.08;
+    return Container(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          // Calculate 20% of the width for padding
+          double horizontalPadding = constraints.maxWidth * 0.05;
+          double h = constraints.maxHeight * 0.08;
 
-
-        return Padding(
-          padding: EdgeInsets.symmetric(horizontal: horizontalPadding,vertical: h),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Color(0xffF8FBFF),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Image(
-                        image: AssetImage('assets/speech-bubble.png',),
-                        height: 25, // Uncomment if you want to specify the height
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10), // Add some space between the rows
-                Row(
-                  children: [
-                    Text(
-                      'Feedback',
-                      style: FontFamily.styleb,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                          'Please rate your experience below',
-                          
-                          
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey[700],
+          return Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding, vertical: h),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color(0xffF8FBFF),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Image(
+                          image: AssetImage(
+                            'assets/speech-bubble.png',
                           ),
-                        )
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: RatingBar.builder(
-                         initialRating: 3,
-                         minRating: 1,
-                         direction: Axis.horizontal,
-                         allowHalfRating: true,
-                         itemCount: 5,
-                         itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                         itemBuilder: (context, _) => Icon(
-                           Icons.star_rounded,
-                           color: Colors.amber,
-                         ),
-                         onRatingUpdate: (rating) {
-                           print(rating);
-                         },
+                          height:
+                              25, // Uncomment if you want to specify the height
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
+                    ],
+                  ),
+                  SizedBox(height: 10), // Add some space between the rows
+                  Row(
+                    children: [
+                      Text(
+                        'Feedback',
+                        style: FontFamily.styleb,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        'Please rate your experience below',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[700],
+                        ),
+                      )),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RatingBar.builder(
+                          initialRating: 5,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star_rounded,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
 
-                Row(children: [Text('Additional feedback',style: FontFamily.styleb.copyWith(fontSize: 16,),)],),
-                SizedBox(height: 5),
-                
-                        // Current Password
-                        FeedbackTextField(),
-                SizedBox(height: 50),
-                        
-                        Row(
-                          children: [
-                            Expanded(
-                              
-                              child: MaterialButton(
-shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(25)),
-                                height: 60,
-                                color: ColorPage.mainBlue,
-                                onPressed: (){},child: Text('Submit feedback',style: FontFamily.style.copyWith(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w800),),),
-                            ),
-                          ],
-                        )
-              ],
+                  Row(
+                    children: [
+                      Text(
+                        'Additional feedback',
+                        style: FontFamily.styleb.copyWith(
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 5),
+
+                  // Current Password
+                  FeedbackTextField(),
+                  SizedBox(height: 50),
+
+                  Row(
+                    children: [
+                      Expanded(
+                        child: MaterialButton(
+                          shape: ContinuousRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          height: 60,
+                          color: ColorPage.mainBlue,
+                          onPressed: () {},
+                          child: Text(
+                            'Submit feedback',
+                            style: FontFamily.style.copyWith(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
@@ -151,7 +176,7 @@ class FeedbackTextField extends StatelessWidget {
         initialValue: 'My feedback!!', // Initial text as per the image
         maxLines: 4, // Adjust the number of lines as needed
         decoration: InputDecoration(
-          // labelText: 'Additional feedback', 
+          // labelText: 'Additional feedback',
           alignLabelWithHint: true, // Align label with the hint text
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10), // Rounded corners
@@ -162,7 +187,8 @@ class FeedbackTextField extends StatelessWidget {
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10), // Rounded corners
             borderSide: BorderSide(
-              color: Colors.grey.withOpacity(0.5), // Grey color for enabled border
+              color:
+                  Colors.grey.withOpacity(0.5), // Grey color for enabled border
             ),
           ),
           focusedBorder: OutlineInputBorder(
@@ -173,7 +199,9 @@ class FeedbackTextField extends StatelessWidget {
             ),
           ),
         ),
-        style: TextStyle(fontSize: 16, color: Colors.grey[800]), // Text style inside the field
+        style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[800]), // Text style inside the field
       ),
     );
   }
